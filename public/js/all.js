76e3afcd529b4912ff92efe10e9d953002eddf9f
Mark Pemburn <mark@pemburn.com>
2017-01-03 11:27:42 -0500

    Fix JS error by adding test for presence of member_update form.

--- public/js/all.js
+++ public/js/all.js
@@ -63,93 +63,96 @@ var FieldToggle = {
 // JS code for Member Edit page
 
 $(document).ready(function ($) {
-    $('.date-pick').datepicker({
-        format: 'M d, yyyy',
-        orientation: 'bottom'
-    });
 
-    $('[name=phone_button]').on('click', function() {
-        var value = $(this).val();
-        $('[name=Primary_Phone]').val(value);
-    });
-
-    /* Use FieldToggle to toggle visibility of date fields */
-    var toggler = Object.create(FieldToggle);
-    $('#member_degree').on('change', function () {
-        toggler.doToggle({
-            toggleType: 'select_multi',
-            actorSelector: '#member_degree',
-            actionSelector: '.degree-date',
-            multiAttribute: 'data-degree-date'
+    if ($('#member_update').is('*')) {
+        $('.date-pick').datepicker({
+            format: 'M d, yyyy',
+            orientation: 'bottom'
         });
-    })
 
-    $('#bonded_check').on('click', function () {
-        toggler.doToggle({
-            toggleType: 'checkbox',
-            actorSelector: '#' + $(this).attr('id'),
-            actionSelector: '.form-group.bonded-date'
+        $('[name=phone_button]').on('click', function() {
+            var value = $(this).val();
+            $('[name=Primary_Phone]').val(value);
         });
-    });
 
-    $('#solitary_check').on('click', function () {
-        toggler.doToggle({
-            toggleType: 'checkbox',
-            actorSelector: '#' + $(this).attr('id'),
-            actionSelector: '.form-group.solitary-date'
-        });
-    });
-
-    $('#leadership-role').on('change', function () {
-        toggler.doToggle({
-            toggleType: 'select',
-            actorSelector: '#' + $(this).attr('id'),
-            actionSelector: '.form-group.leadership-date',
-            emptyValue: '0'
-        });
-    });
-
-    $('#board-role').on('change', function () {
-        toggler.doToggle({
-            toggleType: 'select',
-            actorSelector: '#' + $(this).attr('id'),
-            actionSelector: '.form-group.expiry-date',
-            emptyValue: ''
-        });
-    });
-
-    /* Detect any changes to the form data */
-    $('#member_update').dirtyForms()
-        .on('dirty.dirtyforms clean.dirtyforms', function (ev) {
-            var $form = $(ev.target);
-            var $submitButton = $form.find('[type="submit"]');
-            if (ev.type === 'dirty') {
-                $submitButton.removeAttr('disabled');
-            } else {
-                $submitButton.attr('disabled', 'disabled');
-            }
-    });
-    /* Submit form via AJAX */
-    $('#member_update').on('submit', function (e) {
-        var formAction = this.action;
-        $.ajaxSetup({
-            header: $('meta[name="_token"]').attr('content')
-        });
-        e.preventDefault(e);
-
-        $.ajax({
-            type: "POST",
-            url: formAction,
-            data: $(this).serialize(),
-            dataType: 'json',
-            success: function (data) {
-                console.log(data);
-            },
-            error: function (data) {
-                console.log(data);
-            }
+        /* Use FieldToggle to toggle visibility of date fields */
+        var toggler = Object.create(FieldToggle);
+        $('#member_degree').on('change', function () {
+            toggler.doToggle({
+                toggleType: 'select_multi',
+                actorSelector: '#member_degree',
+                actionSelector: '.degree-date',
+                multiAttribute: 'data-degree-date'
+            });
         })
-    });
+
+        $('#bonded_check').on('click', function () {
+            toggler.doToggle({
+                toggleType: 'checkbox',
+                actorSelector: '#' + $(this).attr('id'),
+                actionSelector: '.form-group.bonded-date'
+            });
+        });
+
+        $('#solitary_check').on('click', function () {
+            toggler.doToggle({
+                toggleType: 'checkbox',
+                actorSelector: '#' + $(this).attr('id'),
+                actionSelector: '.form-group.solitary-date'
+            });
+        });
+
+        $('#leadership-role').on('change', function () {
+            toggler.doToggle({
+                toggleType: 'select',
+                actorSelector: '#' + $(this).attr('id'),
+                actionSelector: '.form-group.leadership-date',
+                emptyValue: '0'
+            });
+        });
+
+        $('#board-role').on('change', function () {
+            toggler.doToggle({
+                toggleType: 'select',
+                actorSelector: '#' + $(this).attr('id'),
+                actionSelector: '.form-group.expiry-date',
+                emptyValue: ''
+            });
+        });
+
+        /* Detect any changes to the form data */
+        $('#member_update').dirtyForms()
+            .on('dirty.dirtyforms clean.dirtyforms', function (ev) {
+                var $form = $(ev.target);
+                var $submitButton = $form.find('[type="submit"]');
+                if (ev.type === 'dirty') {
+                    $submitButton.removeAttr('disabled');
+                } else {
+                    $submitButton.attr('disabled', 'disabled');
+                }
+            });
+        /* Submit form via AJAX */
+        $('#member_update').on('submit', function (e) {
+            var formAction = this.action;
+            $.ajaxSetup({
+                header: $('meta[name="_token"]').attr('content')
+            });
+            e.preventDefault(e);
+
+            $.ajax({
+                type: "POST",
+                url: formAction,
+                data: $(this).serialize(),
+                dataType: 'json',
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function (data) {
+                    console.log(data);
+                }
+            })
+        });
+    }
 });
 
 // JS code for Member List page
