4674d1501542dafc5d6a4d66bc1d9af707289446
Mark Pemburn <mark@pemburn.com>
2017-01-09 19:36:05 -0500

    Configure mail.php with 'from' address.

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@sacredwheel.org', 'name' => 'noreply'],
 
     /*
     |--------------------------------------------------------------------------
